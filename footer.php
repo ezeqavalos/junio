1a816195e4e8e91e9560e5dc4def9dbdcfeacf4a
Felix Barros <soyFelixBarros@gmail.com>
2016-06-01 23:04:15 -0300

    Menu pie

--- footer.php
+++ footer.php
@@ -7,9 +7,21 @@
  * @since Junio 0.0.0
  */
 ?>
-		<footer role="contentinfo">
+		<footer role="contentinfo" class="text-center">
 			<div class="row">
-				<div class="col-sm-5 col-md-6">
+				<div class="col-md-12">
+					<ul class="list-inline">
+						<li><a href="#">Inicio</a></li>
+						<li><a href="#">Novedades</a></li>
+						<li><a href="#">Quiero ser voluntario</a></li>
+						<li><a href="#">Contactar</a></li>
+						<li><a href="#">Perchero Solidario</a></li>
+					</ul>
+				</div>
+
+				<hr />
+				
+				<div class="col-md-12">
 					<p class="copyright">&copy; <?php echo date('Y'); ?> Somos Uno. Todos los derechos reservados.</p>
 				</div>
 			</div><!-- .row -->
@@ -17,8 +29,8 @@
 
 	</div><!-- .container -->
 
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.12.4.js"></script>
-	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.js"></script>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery-1.12.4.js"></script>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/bootstrap.js"></script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -15,17 +15,17 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/images/favicon.ico" type="image/x-icon">
 	<link href='https://fonts.googleapis.com/css?family=Cabin:400,600|Open+Sans:400,600' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap.css">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/style.css">
 </head>
 
 <body>
 
 	<header role="banner">
-		
 		<div class="very-top-header">
 			<div class="container">
 				<div class="row">
@@ -43,7 +43,7 @@
 		<nav class="navbar navbar-default" role="navigation">
 			<div class="container">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="#"><img alt="Mini logo de Somos Uno" width="20" height="20" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAA81BMVEX///9VPnxWPXxWPXxWPXxWPXxWPXxWPXz///9hSYT6+vuFc6BXPn37+vz8+/z9/f2LeqWMe6aOfqiTg6uXiK5bQ4BZQX9iS4VdRYFdRYJfSINuWI5vWY9xXJF0YJR3Y5Z4ZZd5ZZd6Z5h9apq0qcW1qsW1q8a6sMqpnLyrn76tocCvpMGwpMJoUoprVYxeRoJjS4abjLGilLemmbrDutDFvdLPx9nX0eDa1OLb1uPd1+Td2OXe2eXh3Ofj3+nk4Orl4evp5u7u7PLv7fPx7/T08vb08/f19Pf29Pj39vn6+fuEcZ9YP35aQn/8/P1ZQH5fR4PINAOdAAAAB3RSTlMAIWWOw/P002ipnAAAAPhJREFUeF6NldWOhEAUBRvtRsfdfd3d3e3/v2ZPmGSWZNPDqScqqaSBSy4CGJbtSi2ubRkiwXRkBo6ZdJIApeEwoWMIS1JYwuZCW7hc6ApJkgrr+T/eW1V9uKXS5I5GXAjW2VAV9KFfSfgJpk+w4yXhwoqwl5AIGwp4RPgdK3XNHD2ETYiwe6nUa18f5jYSxle4vulw7/EtoCdzvqkPv3bn7M0eYbc7xFPXzqCrRCgH0Hsm/IjgTSb04W0i7EGjz+xw+wR6oZ1MnJ9TWrtToEx+4QfcZJ5X6tnhw+nhvqebdVhZUJX/oFcKvaTotUcvUnY188ue/n38AunzPPE8yg7bAAAAAElFTkSuQmCC"></a>
+					<h1 class="logo"><a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				</div><!-- .navbar-header -->
 
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -57,9 +57,6 @@
 				</div><!-- .collapse -->
 			</div><!-- .container -->
 		</nav>
-
 	</header>
-
-
 
 	<div class="container">
