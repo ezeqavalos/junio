524e037581bd71bba0edbe60122a5ba28e4da2d6
Felix Barros <soyFelixBarros@gmail.com>
2016-06-04 03:26:52 -0300

    Created page 404

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * La plantilla para la visualización de las páginas 404 (no encontrado).
+ *
+ * @package WordPress
+ * @subpackage Junio
+ * @since Junio 0.0
+ */
+
+get_header(); ?>
+
+	<div class="container">
+		<div class="row" role="main">
+			<div class="col-md-12 text-center">
+				<header>
+					<h1><?php _e( '404', 'junio' ); ?></h1>
+				</header>
+
+				<div class="page-content">
+					<p><?php _e( 'Parece que no se encontró nada en este lugar.', 'junio' ); ?></p>
+				</div><!-- .page-content -->
+			</div>
+		</div><!-- .row -->
+	</div><!-- .container -->
+
+<?php get_footer();
