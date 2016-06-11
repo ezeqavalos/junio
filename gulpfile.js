cb96246323eb84ae74862c6353824cd6f375a17b
Felix Barros <soyFelixBarros@gmail.com>
2016-06-11 02:02:54 -0300

    Add gulp-notify

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@ gulp.task('styles', function () {
 		.pipe(plugins.autoprefixer({ browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'] }))
 		.pipe(gulp.dest('./assets/styles/build'))
 		.pipe(plugins.minifyCss({ keepSpecialComments: 1 }))
-		.pipe(gulp.dest('./'));
+		.pipe(gulp.dest('./'))
+		.pipe(plugins.notify({ message: 'Styles tarea completa' }));
 });
 
 gulp.task('default', ['styles']); 
