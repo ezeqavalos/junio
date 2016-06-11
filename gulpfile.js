60f886a95f4ef6573748dc25e972442ef878bf03
Felix Barros <soyFelixBarros@gmail.com>
2016-06-11 00:38:20 -0300

    Add gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp    = require('gulp'),
 	plugins = require('gulp-load-plugins')();
 
+// Compilar CSS a partir de archivos Sass
 gulp.task('styles', function () {
 
 	gulp.src('./css/*.scss')
-		.pipe(plugins.sass())
+		.pipe(plugins.sass({ style: 'expanded', compass: true }))
+		.pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
 		.pipe(gulp.dest('./'));
 });
 
