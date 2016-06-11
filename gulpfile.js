47dcdf936adbc697d47a59b979d1af03c56a6d43
Felix Barros <soyFelixBarros@gmail.com>
2016-06-11 01:57:37 -0300

    Refactorizamos el css

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,12 @@ var gulp    = require('gulp'),
 // Compilar CSS a partir de archivos Sass
 gulp.task('styles', function () {
 
-	gulp.src('./css/*.scss')
+	gulp.src('./assets/styles/**/*.scss')
 		.pipe(plugins.sass({ style: 'expanded', compass: true }))
-		.pipe(plugins.autoprefixer('last 2 versions', 'ie 9', 'ios 6', 'android 4'))
+		.pipe(plugins.autoprefixer({ browsers: ['last 2 versions', 'ie 9', 'ios 6', 'android 4'] }))
+		.pipe(gulp.dest('./assets/styles/build'))
+		.pipe(plugins.minifyCss({ keepSpecialComments: 1 }))
 		.pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['styles']);
+gulp.task('default', ['styles']); 
